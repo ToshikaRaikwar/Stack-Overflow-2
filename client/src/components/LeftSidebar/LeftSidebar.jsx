821d1f3e611a85e13029783aae98e1371240454c
Toshika Raikwar <95243378+ToshikaRaikwar@users.noreply.github.com>
2023-07-03 21:07:54 +0530

    Update LeftSidebar.jsx

--- client/src/components/LeftSidebar/LeftSidebar.jsx
+++ client/src/components/LeftSidebar/LeftSidebar.jsx
@@ -58,7 +58,7 @@ const LeftSidebar = ({ slideIn, handleSlideIn }) => {
               style={{ paddingLeft: "40px" }}
             >
               <p>Users</p>
-            </NavLink>
+            </NavLink> 
             <NavLink to="https://6497311cce5f6e0617761084--chic-gumption-a6267f.netlify.app/" className="side-nav-links" activeClassName="active" style={{ paddingLeft: "40px" }}>
            <img src={shop} alt="shop" width="30px" height="30px" />
             <p style={{ paddingLeft: "10px" }}>Buy</p>
